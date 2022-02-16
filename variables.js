6b9d21723031ffacc28f15bf67b8c93aba8d3913
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-02-17 00:26:47 +0530

    first day,first commit

---
+++ variables.js
@@ -0,0 +1,11 @@
+let firstname='shahnawaz'
+    lastname='alam'
+    age=18
+    country='india'
+console.log(firstname,lastname,age,country)
+console.log(typeof firstname)
+console.log(typeof age)
+let something
+console.log(something)
+let ismaried= false
+console.log(ismaried)
