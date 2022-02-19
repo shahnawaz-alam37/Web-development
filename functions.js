e5f147532ace7d2f383d357021fb82e0bcf560b0
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-02-20 00:00:36 +0530

    added functions to buttons

---
+++ functions.js
@@ -0,0 +1,11 @@
+document.getElementById('head').onclick=changecolor;
+function changecolor(){
+    document.body.style.color="purple";
+}
+
+document.getElementById('cred').onclick=function(){
+    document.getElementById('body').style.color = "red";
+}
+document.getElementById('cgreen').onclick=function(){
+    document.getElementById("body").style.color = "green";
+}
