1a17aaed9a47209f40485aa881240929e59d6532
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-02-22 00:10:00 +0530

    function to change background color

--- functions.js
+++ functions.js
@@ -9,3 +9,11 @@ document.getElementById('cred').onclick=function(){
 document.getElementById('cgreen').onclick=function(){
     document.getElementById("body").style.color = "green";
 }
+
+document.getElementById('black').onclick= black;
+function black(){
+    document.getElementById('body').style.backgroundColor='black';
+}
+function gray() {
+    document.getElementById('body').style.backgroundColor='gray';
+}
