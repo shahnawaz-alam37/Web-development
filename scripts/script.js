3025fcf08f096c5d841481daacf3234f128d5118
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-08-20 23:36:55 +0530

    updated html file

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-/*document.getElementById("do").onclick=myfun();
-function myfun(){
-document.getElementById("demo").innerHTML=Date();
-}
-*/
-document.getElementById("lop").onclick=mylop;
-function mylop(){
-    document.body.style.background="red";
-}
